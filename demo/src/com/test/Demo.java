b92729eed219e5b49a34cba2c1cc5e236a9c54a2
739224536 <739224536@qq.com>
2018-12-31 16:26:58 +0800

    测试1231

--- demo/src/com/test/Demo.java
+++ demo/src/com/test/Demo.java
@@ -2,46 +2,9 @@ package com.test;
 
 public class Demo {
 	public static void main(String[] args) {
-		
-		
-		
-		
-		
-		
-		
+				
 		System.out.println(1234);
-		
-		
-			System.out.println(1234);
-				System.out.println(1234);
-					System.out.println(1234);
-					
-					
-					
-					
-					
-					
-					
-					
-					
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 		
 	}
 
